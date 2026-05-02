a614250fa57b6ee3bb665ea32f3ce6f3a2000640
chominap12 <chominapoudel@gmail.com>
2026-05-02 08:48:33 +0545

    update app.jsx

--- react-project/counter-app/src/App.jsx
+++ react-project/counter-app/src/App.jsx
@@ -2,20 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
- 
-
+  const [count,setcount]= useState(0);
+  
   return (
-    <>
+    <div>
+       <h1>Counter App</h1>
+       <button onClick={setcount}>+</button>
+
+
+
+    </div>
       
      
        
               
              
           
-    </>
+    
   )
 }
 
