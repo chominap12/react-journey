50ff4320aff5595fd3db8e5cce0a193c450b7a32
chominap12 <chominapoudel@gmail.com>
2026-05-02 20:47:20 +0545

    update app.jsx

--- react-project/calculator-app/src/App.jsx
+++ react-project/calculator-app/src/App.jsx
@@ -5,14 +5,53 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  
-
+      const [input,setInput]=useState("");
+       const handleClick =(value)=>{
+        if(value=="="){
+          try{
+          setInput(eval(input).toString());
+        }catch{
+              setInput("Error");
+                }
+         }
+         else if(value=="X"){
+          setInput((prev)=>prev.slice(0,-1));
+         }
+       else{
+        setInput((prev)=> prev+value);
+              }
+         }
+    const clear = ()=>{
+      setInput("");
+    }
+    
   return (
-    <>
-      
-              
-     
-    </>
+    <div>
+      <h1>{input}</h1>
+       <button onClick={clear}>C</button>
+       <button onClick={()=>handleClick("X")}>X</button>
+       <button onClick={()=>handleClick("%")}>%</button>
+       <button onClick={()=>handleClick("/")}>/</button>
+       <br></br>
+       <button onClick={()=>handleClick("7")}>7</button>
+       <button onClick={()=> handleClick("8")}>8</button>
+       <button onClick={()=>handleClick("9")}>9</button>
+       <button onClick={()=> handleClick("*")}>*</button>
+        <br/>
+        <button onClick={()=> handleClick("4")}>4</button>  
+        <button onClick={()=> handleClick("5")}>5</button> 
+        <button onClick={()=> handleClick("6")}>6</button>
+        <button onClick={()=> handleClick("-")}>-</button>
+        <br/>
+        <button onClick={()=>handleClick("1")}>1</button>
+        <button onClick={()=> handleClick("2")}>2</button>
+        <button onClick={()=> handleClick("3")}>3</button>
+        <button onClick={()=> handleClick("+")}>+</button>
+        <br/>
+        <button onClick={()=> handleClick("0")}>0</button>
+        <button onClick={()=> handleClick(".")}>.</button>
+        <button onClick={()=>handleClick("=")}>=</button>     
+    </div>
   )
 }
 
