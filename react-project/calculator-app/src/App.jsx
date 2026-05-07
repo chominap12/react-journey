940c511cea5d9a194fa37dda0c1a3c236af31f9f
chominap12 <chominapoudel@gmail.com>
2026-05-07 21:40:24 +0545

    update calculator app

--- react-project/calculator-app/src/App.jsx
+++ react-project/calculator-app/src/App.jsx
@@ -37,30 +37,39 @@ function App() {
     
   return (
     <div>
+
       <h1>{input}</h1>
-       <button onClick={clear}>C</button>
-       <button onClick={()=>handleClick("X")}>X</button>
-       <button onClick={()=>handleClick("%")}>%</button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4,1fr)",
+          gap: "10px",
+        }}
+      >
+       <button style={{padding:"15px"}} onClick={clear}>C</button>
+       <button style={{padding:"15px"}} onClick={()=>handleClick("X")}>X</button>
+       <button  style={{padding:"15px"}} onClick={()=>handleClick("%")}>%</button>
        <button onClick={()=>handleClick("/")}>/</button>
-       <br></br>
+       
        <button onClick={()=>handleClick("7")}>7</button>
        <button onClick={()=> handleClick("8")}>8</button>
        <button onClick={()=>handleClick("9")}>9</button>
        <button onClick={()=> handleClick("*")}>*</button>
-        <br/>
+       
         <button onClick={()=> handleClick("4")}>4</button>  
         <button onClick={()=> handleClick("5")}>5</button> 
         <button onClick={()=> handleClick("6")}>6</button>
         <button onClick={()=> handleClick("-")}>-</button>
-        <br/>
+       
         <button onClick={()=>handleClick("1")}>1</button>
         <button onClick={()=> handleClick("2")}>2</button>
         <button onClick={()=> handleClick("3")}>3</button>
         <button onClick={()=> handleClick("+")}>+</button>
-        <br/>
+       
         <button onClick={()=> handleClick("0")}>0</button>
         <button onClick={()=> handleClick(".")}>.</button>
         <button onClick={()=>handleClick("=")}>=</button>     
+    </div>
     </div>
   )
 }
